d6bda76adccba7829f07c4fd7ff995bff81d5742
Nikita <mikitavydrankou@gmail.com>
2025-07-21 14:01:19 +0200

    Waluta, SloKursyWaluty

---
+++ DXApplication/DXApplication.Module/BusinessObjects/SloKursyWaluty.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Xpo;
+using System;
+using System.ComponentModel;
+
+namespace DXApplication.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [Persistent("SLO_KURSY_WALUTY")]
+    public class SloKursyWaluty : XPLiteObject
+    {
+        public SloKursyWaluty(Session session) : base(session) { }
+
+        [Key(true)]
+        [Persistent("SLO_KURSY_WALUTY_ID")]
+        [Browsable(false)]
+        public int SLO_KURSY_WALUTY_ID { get; set; }
+
+        [Persistent("KURS_Z_DNIA")]
+        public DateTime KursZDnia { get; set; }
+
+        [Persistent("NUMER_TABELI")]
+        public string NumerTabeli { get; set; }
+
+        [Persistent("KURS")]
+        public decimal Kurs { get; set; }
+
+        [Persistent("DATA_OD")]
+        public DateTime WazneOd { get; set; }
+
+        [Persistent("DATA_DO")]
+        public DateTime WazneDo { get; set; }
+
+        [Association("Waluta-KursyWalut")]
+        [Persistent("WALUTA_ID")] 
+        public Waluta Waluta { get; set; }
+
+    }
+}
---
+++ DXApplication/DXApplication.Module/BusinessObjects/Waluta.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Xpo;
+using System.ComponentModel;
+
+namespace DXApplication.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [Persistent("WALUTA")]
+    [DefaultProperty("WALUTA_KOD")]
+    public class Waluta : XPLiteObject
+    {
+        public Waluta(Session session) : base(session) { }
+
+        [Key(true)]
+        [Persistent("WALUTA_ID")]
+        [Browsable(false)]
+        public int WALUTA_ID { get; set; }
+
+        [Persistent("WALUTA_KOD")] 
+        public string WALUTA_KOD { get; set; }
+
+        [Persistent("WALUTA_OPIS")]
+        public string WALUTA_OPIS { get; set; }
+
+        [Association("Waluta-KursyWalut")]
+        public XPCollection<SloKursyWaluty> KursyWalut
+        {
+            get { return GetCollection<SloKursyWaluty>(nameof(KursyWalut)); }
+        }
+    }
+}
