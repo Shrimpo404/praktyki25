d1850c4bccb369159a396c23625e3392f663e554
Nikita <mikitavydrankou@gmail.com>
2025-07-23 11:27:32 +0200

    słownik TAGI added

---
+++ DXApplication/DXApplication.Module/BusinessObjects/Tagi.cs
@@ -0,0 +1,28 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Xpo;
+using System.ComponentModel;
+
+namespace DXApplication.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [Persistent("TAGI")]
+    [DefaultProperty("TAGI_KOD")]
+    public class Tagi : XPLiteObject
+    {
+        public Tagi(Session session) : base(session) { }
+
+        [Key(true)]
+        [Persistent("TAGI_ID")]
+        [Browsable(false)]
+        public int TAGI_ID { get; set; }
+
+        [Persistent("TAGI_KOD")]
+        public string TAGI_KOD { get; set; }
+
+        [Persistent("TAGI_OPIS")]
+        public string TAGI_OPIS { get; set; }
+
+        [Persistent("KOLEJNOSC")]
+        public string KOLEJNOSC { get; set; }
+    }
+}
